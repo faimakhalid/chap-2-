7608274f5f2a284ca66075a2752e49002a33c981
faimakhalid <fatimakhalidjutt@gmail.com>
2025-01-13 23:23:42 +0500

    Add files via upload

---
+++ chap 2/script.js
@@ -0,0 +1,13 @@
+// Question 1
+// Greet the website visitor
+alert("Welcome to my website!");
+
+// Question 2
+// Display an error message
+alert("Error! Please enter a valid password.");
+
+// Question 3
+// Display a message with line breaks
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Question 4
